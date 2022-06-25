1f239bf1a8f9226072cf88ecdd8a300e536f8ed1
iqbalfanani <iiqbal1809@gmail.com>
2022-06-25 20:45:50 +0700

    update api route solver

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,25 @@ module.exports = function (app) {
     
   app.route('/api/solve')
     .post((req, res) => {
+      const { puzzle } = req.body;
+    if (!puzzle) {
+      res.json({ error: "Required field missing" });
+      return;
+    }
+    if (puzzle.length != 81) {
+      res.json({ error: "Expected puzzle to be 81 characters long" });
+      return;
+    }
+    if (/[^0-9.]/g.test(puzzle)) {
+      res.json({ error: "Invalid characters in puzzle" });
+      return;
+    }
+    let solvedString = solver.solve(puzzle);
+    if (!solvedString) {
+      res.json({ error: "Puzzle cannot be solved" });
+    } else {
+      res.json({ solution: solvedString });
+    }
 
     });
 };
